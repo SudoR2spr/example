5546b86ad2e39755cebd49e4fccaa2a7ca2481f6
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-11-15 11:24:54 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='encoded_op',  # The name of the package
+    name='encoded',  # The name of the package
     version='0.1',
-    py_modules=['encoded_op'],
+    py_modules=['encoded'],
     install_requires=[
         'requests',  # Your other dependencies
         'Flask',
